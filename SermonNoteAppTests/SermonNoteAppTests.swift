33e3702aa04b19b4cc50b3b31f158247902dc1b9
박찬누리 <noori@uniquegood.biz>
2025-04-18 10:30:38 +0900

    Initial Commit

---
+++ SermonNoteAppTests/SermonNoteAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SermonNoteAppTests.swift
+//  SermonNoteAppTests
+//
+//  Created by 박찬누리 on 4/18/25.
+//
+
+import Testing
+@testable import SermonNoteApp
+
+struct SermonNoteAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
